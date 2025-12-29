fd5676f3aed4f028fc9b14303308c80355a650c0
YAMADOGS <yamadogsnft@gmail.com>
2025-12-30 07:36:29 +0800

    Add files via upload

--- mint.js
+++ mint.js
@@ -303,7 +303,7 @@ updateMintCounter();
 
 if (tokenId) {
   showToast(`
-    🎉 Mint success! Your YAMADOGS is now part of the pack and ready for some pup-tastic journeys!<b>YAMADOGS #${tokenId}</b>🐾<br>
+    🎉 Mint success! Your YAMADOGS is now part of the pack and ready for some pup-tastic journeys!🐾<b>YAMADOGS #${tokenId}</b><br>
     <a href="#" id="viewNftLink" style="color:#ffb703; text-decoration:underline;">
       🔗 View on Sepolia
     </a>
